a3a888fc4d202f397195b4866b2d6fe9cf17efac
Takeyuki0617 <janjagajan0@gmail.com>
2020-05-23 10:33:12 +0900

    feat: 最初のページ作成

---
+++ which_my_dog/lib/main.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              '愛犬どっち？',
+              style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
+            ),
+            Image.asset(
+              'images/inu_silhouette.png',
+              width: 300,
+              height: 300,
+            ),
+            SizedBox(
+              width: 300,
+              height: 80,
+              child: RaisedButton(
+                child: Text(
+                  "スタート",
+                  style: TextStyle(fontSize: 30),
+                ),
+                onPressed: () {},
+              ),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
