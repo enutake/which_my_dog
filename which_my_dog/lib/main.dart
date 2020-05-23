6c82223b41c4ec6b68619cf50b9a615345da2c03
Takeyuki0617 <janjagajan0@gmail.com>
2020-05-23 11:19:25 +0900

    feat: ルーティング処理をroutesでの管理に変更

--- which_my_dog/lib/main.dart
+++ which_my_dog/lib/main.dart
@@ -9,29 +9,16 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'Which My Dog',
+      initialRoute: '/',
+      routes: {
+        '/': (context) => FirstPage(),
+      },
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
+class FirstPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
