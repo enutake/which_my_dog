75b17622e082d8c1781a0efcf8043196ffc9244c
Takeyuki0617 <janjagajan0@gmail.com>
2020-05-23 19:30:16 +0900

    feat: 漢字が中国語だったのでロケールを日本語に変更

--- which_my_dog/lib/main.dart
+++ which_my_dog/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,6 +10,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      localizationsDelegates: [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+      ],
+      supportedLocales: [
+        Locale('ja', 'JP'),
+      ],
       title: 'Which My Dog',
       initialRoute: '/',
       routes: {
