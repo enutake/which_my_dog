343812057a5fc7a6529692d3315e3ad119aafcb0
Takeyuki0617 <janjagajan0@gmail.com>
2020-05-24 22:53:58 +0900

    feat: 画像サイズに制限を設けてエラーに対処

--- which_my_dog/lib/pages/quiz_page.dart
+++ which_my_dog/lib/pages/quiz_page.dart
@@ -12,34 +12,36 @@ class QuizPage extends StatelessWidget {
     return Scaffold(
       body: Center(
         child: Consumer<MyAPPModel>(
-          builder: (context, model, _)
-        {
-          return Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Align(
-                alignment: Alignment.centerRight,
-                child: Padding(
-                  padding: const EdgeInsets.all(10.0),
-                  child: Text(
-                    '${model.currentQuestionCount}/$quizMaxLength',
-                    style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+          builder: (context, model, _) {
+            return Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Align(
+                  alignment: Alignment.centerRight,
+                  child: Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Text(
+                      '${model.currentQuestionCount}/$quizMaxLength',
+                      style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                    ),
                   ),
                 ),
-              ),
-              Image.asset(
-                'images/${model.quizList[model.currentQuestionCount - 1]}.jpg',
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  CircleButton(icon: Icons.clear, color: Colors.purple),
-                  CircleButton(icon: Icons.favorite, color: Colors.red),
-                ],
-              ),
-            ],
-          );
-        }
+                ConstrainedBox(
+                  constraints: BoxConstraints(maxWidth: 500, maxHeight: 600),
+                  child: Image.asset(
+                    'images/${model.quizList[model.currentQuestionCount - 1]}.jpg',
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    CircleButton(icon: Icons.clear, color: Colors.purple),
+                    CircleButton(icon: Icons.favorite, color: Colors.red),
+                  ],
+                ),
+              ],
+            );
+          }
         )
       ),
     );
